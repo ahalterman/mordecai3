a68bc3eab68e1c0f27388aacbbc262ef493a7577
Andy <ahalterman0@gmail.com>
2023-03-21 15:20:31 -0400

    Add setup.py for eventual pypi release

--- setup.py
+++ setup.py
@@ -2,4 +2,44 @@ from setuptools import setup, find_packages
 setup(
     name = 'mordecai3',
     version='3.0.0a',
+    url='http://github.com/ahalterman/mordecai3/',
+    author='Andy Halterman',
+    author_email='ahalterman0@gmail.com',
+    license='MIT',
+    packages=['mordecai3'],
+    keywords = ['geoparsing', 'nlp', 'geocoding', 'toponym resolution'],
+    package_dir = {
+        "mordecai3": "mordecai3"
+    }
 )
+
+
+#from setuptools import setup
+#
+#setup(name='mordecai',
+#      version='2.1.0',
+#      description='Full text geoparsing and event geocoding',
+#      url='http://github.com/openeventdata/mordecai/',
+#      author='Andy Halterman',
+#      author_email='ahalterman0@gmail.com',
+#      license='MIT',
+#      packages=['mordecai'],
+#      keywords = ['geoparsing', 'nlp', 'geocoding', 'toponym resolution'],
+#      install_requires = ['editdistance>=0.5.3',
+#                          'elasticsearch==5.4.0',
+#                          'elasticsearch-dsl==5.3.0',
+#                          'h5py>=2.10.0',
+#                          'pandas>=0.24.2',
+#                          'spacy>=2.3,<3.0',
+#                          'tensorflow>=2.2.0',
+#                          'tqdm>=4.28.1',
+#                          'numpy>=1.12'],
+#      dependency_links=['https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.3.1/en_core_web_lg-2.3.1.tar.gz'],
+#      include_package_data=True,
+#      package_data = {'data': ['admin1CodesASCII.json',
+#                             'countries.json',
+#                             'nat_df.csv',
+#                             'stopword_country_names.json'],
+#                    'models' : ['country_model.h5',
+#                                'rank_model.h5']}
+#     )
