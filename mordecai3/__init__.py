9b339e9e1a6143bdae958bed7fe3ffb0bd1e91a8
Andreas Beger <adbeger@gmail.com>
2025-09-09 12:55:41 +0300

    reduce urllib3 and elasticsearch logging verbosity

--- mordecai3/__init__.py
+++ mordecai3/__init__.py
@@ -1,4 +1,10 @@
+
+import logging
+
 from .geoparse import Geoparser
 
 __version__ = "3.0.0"
 
+# Adjust logging levels for urllib3 and elasticsearch to reduce verbosity
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("elasticsearch").setLevel(logging.ERROR)
