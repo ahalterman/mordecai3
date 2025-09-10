30491891ee382d06d2008d42a78b72400a39e659
Andreas Beger <adbeger@gmail.com>
2025-09-10 09:30:21 +0300

    don't override logging levels in init
    Forces package installers to use those log levels; use a logger setup function instead---I guess that's more proper style?

--- mordecai3/__init__.py
+++ mordecai3/__init__.py
@@ -1,10 +1,5 @@
 
-import logging
-
 from .geoparse import Geoparser
 
 __version__ = "3.0.0"
 
-# Adjust logging levels for urllib3 and elasticsearch to reduce verbosity
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.getLogger("elasticsearch").setLevel(logging.ERROR)
