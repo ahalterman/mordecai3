7a9d4526f222b195e381129a93dee2c34568371e
Andreas Beger <adbeger@gmail.com>
2025-09-10 14:23:27 +0300

    add curated-transformers, curated-tokenizers to cache (#33)
    fix model path in cache
    
    fix cached packages in CI

--- mordecai3/utils.py
+++ mordecai3/utils.py
@@ -1,11 +1,6 @@
 
 import spacy
 
-from .mordecai_utilities import spacy_doc_setup
-
-# need so that spacy.load doesn't throw an error about missing factory, #33
-spacy_doc_setup()
-
 def check_spacy_model() -> bool:
     """Check if the spacy model is available."""
     try:
